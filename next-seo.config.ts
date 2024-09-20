45df72825e781ad069df4f391e8154d78ed831ef
alex7842 <alex1958229@gmail.com>
2024-09-20 15:18:25 +0530

    seo optimization

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,24 +1,22 @@
 export const SEO = {
-  title: "ChatPulse",
-  description: "Revolutionise your Learning Experience.",
+  title: "ChatPulse - Your PDF Analyzer",
+  description: "Revolutionise your Learning Experience with ChatPulse. Chat with PDFs and analyze documents effortlessly.",
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "ChatPulse",
-    // // url: "https://ChatPulse.vercel.app/",
-    siteName: "chatpulse",
+    url: "https://www.chatpulse.dev/",
+    siteName: "ChatPulse",
     images: [
       { url: "/logo.png" },
       {
         url: "https://chatpulseai.vercel.app/og.png",
         width: 800,
         height: 600,
-        alt: "Og Image",
+        alt: "ChatPulse - PDF Analyzer",
         type: "image/jpeg",
       },
     ],
   },
-
   themeColor: "light",
   additionalMetaTags: [
     {
@@ -30,7 +28,6 @@ export const SEO = {
       content: "ChatPulse",
     },
   ],
-
   additionalLinkTags: [
     {
       rel: "icon",
